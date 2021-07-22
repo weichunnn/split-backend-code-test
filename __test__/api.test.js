e6b9bac77d7d040d6e96b1e7eaddb522212e8b7d
Wei Chun <tanweichun@outlook.com>
2021-07-22 23:46:11 +0800

    added solutions to assignments

--- __test__/api.test.js
+++ __test__/api.test.js
@@ -1,128 +1,170 @@
-const request = require('supertest')
-const app = require('../app')
+const request = require("supertest");
+const app = require("../app");
 
-describe('One Settlment Per Week API TEST #1 ', () => {
-    it('Testcase 1: expect response paymentDate to be 10-02-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/oneSettlementPerWeek')
-            .send({ "start": "03-02-2020", "end": "05-02-2020" })
-        expect(res.body.paymentDate).toEqual('10-02-2020')
-    })
-})
+describe("One Settlment Per Week API TEST #1 ", () => {
+  it("Testcase 1: expect response paymentDate to be 10-02-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/oneSettlementPerWeek")
+      .send({ start: "03-02-2020", end: "05-02-2020" });
+    expect(res.body.paymentDate).toEqual("10-02-2020");
+  });
+});
 
-describe('One Settlment Per Week API TEST #2 ', () => {
-    it('Testcase 2: expect response paymentDate to be 17-02-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/oneSettlementPerWeek')
-            .send({ "start": "03-02-2020", "end": "10-02-2020" })
-        expect(res.body.paymentDate).toEqual('17-02-2020')
-    })
-})
+describe("One Settlment Per Week API TEST #2 ", () => {
+  it("Testcase 2: expect response paymentDate to be 17-02-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/oneSettlementPerWeek")
+      .send({ start: "03-02-2020", end: "10-02-2020" });
+    expect(res.body.paymentDate).toEqual("17-02-2020");
+  });
+});
 
-describe('One Settlment Per Week API TEST #3 ', () => {
-    it('Testcase 3: expect response paymentDate to be 10-02-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/oneSettlementPerWeek')
-            .send({ "start": "03-02-2020", "end": "06-02-2020" })
-        expect(res.body.paymentDate).toEqual('10-02-2020')
-    })
-})
+describe("One Settlment Per Week API TEST #3 ", () => {
+  it("Testcase 3: expect response paymentDate to be 10-02-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/oneSettlementPerWeek")
+      .send({ start: "03-02-2020", end: "06-02-2020" });
+    expect(res.body.paymentDate).toEqual("10-02-2020");
+  });
+});
 
+describe("One Settlment Per Week API TEST #4 ", () => {
+  it("Testcase 4: expect response paymentDate to be 02-03-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/oneSettlementPerWeek")
+      .send({ start: "01-02-2020", end: "24-02-2020" });
+    expect(res.body.paymentDate).toEqual("02-03-2020");
+  });
+});
 
-describe('One Settlment Per Week API TEST #4 ', () => {
-    it('Testcase 4: expect response paymentDate to be 02-03-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/oneSettlementPerWeek')
-            .send({ "start": "01-02-2020", "end": "24-02-2020" })
-        expect(res.body.paymentDate).toEqual('02-03-2020')
-    })
-})
+describe("Two Settlment Per Week API TEST #1 ", () => {
+  it("Testcase 5: expect response paymentDate to be 06-02-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/twoSettlementPerWeek")
+      .send({ start: "03-02-2020", end: "05-02-2020" });
+    expect(res.body.paymentDate).toEqual("06-02-2020");
+  });
+});
 
+describe("Two Settlment Per Week API TEST #2 ", () => {
+  it("Testcase 6: expect response paymentDate to be 10-02-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/twoSettlementPerWeek")
+      .send({ start: "03-02-2020", end: "07-02-2020" });
+    expect(res.body.paymentDate).toEqual("10-02-2020");
+  });
+});
 
-describe('Two Settlment Per Week API TEST #1 ', () => {
-    it('Testcase 5: expect response paymentDate to be 06-02-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/twoSettlementPerWeek')
-            .send({ "start": "03-02-2020", "end": "05-02-2020" })
-        expect(res.body.paymentDate).toEqual('06-02-2020')
-    })
-})
+describe("Two Settlment Per Week API TEST #3 ", () => {
+  it("Testcase 7: expect response paymentDate to be 13-02-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/twoSettlementPerWeek")
+      .send({ start: "03-02-2020", end: "10-02-2020" });
+    expect(res.body.paymentDate).toEqual("13-02-2020");
+  });
+});
 
-describe('Two Settlment Per Week API TEST #2 ', () => {
-    it('Testcase 6: expect response paymentDate to be 10-02-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/twoSettlementPerWeek')
-            .send({ "start": "03-02-2020", "end": "07-02-2020" })
-        expect(res.body.paymentDate).toEqual('10-02-2020')
-    })
-})
+describe("Two Settlment Per Week API TEST #4 ", () => {
+  it("Testcase 8: expect response paymentDate to be 20-02-2020", async () => {
+    const res = await request(app)
+      .post("/tickets/twoSettlementPerWeek")
+      .send({ start: "03-02-2020", end: "19-02-2020" });
+    expect(res.body.paymentDate).toEqual("20-02-2020");
+  });
+});
 
-describe('Two Settlment Per Week API TEST #3 ', () => {
-    it('Testcase 7: expect response paymentDate to be 13-02-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/twoSettlementPerWeek')
-            .send({ "start": "03-02-2020", "end": "10-02-2020" })
-        expect(res.body.paymentDate).toEqual('13-02-2020')
-    })
-})
+describe("Calculate Settlment Amount TEST #1 ", () => {
+  it("Testcase 9: Expect totalSum to be 521.24", async () => {
+    const res = await request(app)
+      .post("/tickets/calculateSettlementAmount")
+      .send([
+        {
+          ticketId: "TE231FD3-23",
+          price: 100,
+          MDR: 2,
+        },
+        {
+          ticketId: "TE2GES23-23",
+          price: 200,
+          MDR: 4,
+        },
+        {
+          ticketId: "T03GD1023-23",
+          price: 246,
+          MDR: 6,
+        },
+      ]);
+    expect(res.body.totalSum).toEqual(521.24);
+  });
+});
 
-describe('Two Settlment Per Week API TEST #4 ', () => {
-    it('Testcase 8: expect response paymentDate to be 20-02-2020', async () => {
-        const res = await request(app)
-            .post('/tickets/twoSettlementPerWeek')
-            .send({ "start": "03-02-2020", "end": "19-02-2020" })
-        expect(res.body.paymentDate).toEqual('20-02-2020')
-    })
-})
+describe("Calculate Settlment Amount TEST #1 ", () => {
+  it("Testcase 10: Expect totalSum to be 933.76", async () => {
+    const res = await request(app)
+      .post("/tickets/calculateSettlementAmount")
+      .send([
+        {
+          ticketId: "TE231023-23",
+          price: 100,
+          MDR: 2.33,
+        },
+        {
+          ticketId: "KE23D0S3-J3",
+          price: 231,
+          MDR: 5.34,
+        },
+        {
+          ticketId: "LDL40S3-U3",
+          price: 659,
+          MDR: 6.31,
+        },
+      ]);
+    expect(res.body.totalSum).toEqual(933.76);
+  });
+});
 
+// CRUD API Tests
+describe("Tickets CRUD API - Create Test", () => {
+  it("Testcase 11: Expect Success Status", async () => {
+    const res = await request(app).post("/tickets").send({
+      ticketId: "TE231023-23",
+      currency: "SGD",
+      price: 100,
+      MDR: 2.33,
+      travelAgentName: "SPLIT-TEST-AGENT01",
+    });
+    expect(res.status).toEqual(200);
+  });
+});
 
-describe('Calculate Settlment Amount TEST #1 ', () => {
-    it('Testcase 9: Expect totalSum to be 521.24', async () => {
-        const res = await request(app)
-            .post('/tickets/calculateSettlementAmount')
-            .send([
-                {
-                    "ticketId": "TE231FD3-23",
-                    "price" : 100,
-                    "MDR" : 2
-                },
-                {
-                    "ticketId": "TE2GES23-23",
-                    "price" : 200,
-                    "MDR" : 4
-                },
-                {
-                    "ticketId": "T03GD1023-23",
-                    "price" : 246,
-                    "MDR" : 6
-                }
-            ])
-        expect(res.body.totalSum).toEqual(521.24)
-    })
-})
+describe("Tickets CRUD API - Read Test", () => {
+  it("Testcase 12: Expect Success Status", async () => {
+    const res = await request(app).get("/tickets");
+    expect(res.status).toEqual(200);
+    expect(res.body.tickets).toEqual([
+      {
+        ticketId: "TE231023-23",
+        currency: "SGD",
+        price: 100,
+        MDR: 2.33,
+        travelAgentName: "SPLIT-TEST-AGENT01",
+      },
+    ]);
+  });
+});
 
+describe("Tickets CRUD API - Update Test", () => {
+  it("Testcase 13: Expect Success Status", async () => {
+    const res = await request(app).put("/tickets/TE231023-23").send({
+      price: 500,
+    });
+    expect(res.status).toEqual(200);
+  });
+});
 
-describe('Calculate Settlment Amount TEST #1 ', () => {
-    it('Testcase 10: Expect totalSum to be 933.76', async () => {
-        const res = await request(app)
-            .post('/tickets/calculateSettlementAmount')
-            .send([
-                {
-                    "ticketId": "TE231023-23",
-                    "price" : 100,
-                    "MDR" : 2.33
-                  },
-                  {
-                    "ticketId": "KE23D0S3-J3",
-                    "price" : 231,
-                    "MDR" : 5.34
-                  },
-                  {
-                    "ticketId": "LDL40S3-U3",
-                    "price" : 659,
-                    "MDR" : 6.31 
-                  }
-            ])
-        expect(res.body.totalSum).toEqual(933.76)
-    })
-})
+describe("Tickets CRUD API - Delete Test", () => {
+  it("Testcase 14: Expect Success Status", async () => {
+    const res = await request(app).delete("/tickets/TE231023-23");
+    expect(res.status).toEqual(200);
+  });
+});
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,6 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var moment = require('moment')
+var moment = require("moment");
+
+// sqlite3 import and init
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(":memory:");
 
 /* 
 HINT
@@ -10,26 +14,40 @@ https://momentjs.com/
 
 */
 
-router.post('/oneSettlementPerWeek', function(req, res, next) {
-    // use req.body to get JSON of start and end dates. We are only concerned with end dates.
-    let endDate = moment(req.body['end'],'DD-MM-YYYY')
-    
-    //add changes below
-    
-    res.json({"paymentDate":moment().format('DD-MM-YYYY')})
-});
-router.post('/twoSettlementPerWeek', function(req, res, next) {
-    let endDate = moment(req.body['end'],'DD-MM-YYYY')
+// Assignment 1
+router.post("/oneSettlementPerWeek", function (req, res, next) {
+  // use req.body to get JSON of start and end dates. We are only concerned with end dates.
+  let endDate = moment(req.body["end"], "DD-MM-YYYY");
+  //add changes below
+  const paymentDate = moment(endDate).add(1, "weeks").isoWeekday(1);
 
-     //add changes below
-    res.json({"paymentDate":moment().format('DD-MM-YYYY')})
-});
-router.post('/calculateSettlementAmount', function(req, res, next) {
-    //add changes below
-    res.json({"totalSum": 123})
+  res.json({ paymentDate: moment(paymentDate).format("DD-MM-YYYY") });
 });
 
+router.post("/twoSettlementPerWeek", function (req, res, next) {
+  let endDate = moment(req.body["end"], "DD-MM-YYYY");
 
+  //add changes below
+  var paymentDate = "";
+  moment(endDate).isoWeekday() <= 3
+    ? (paymentDate = moment(endDate).isoWeekday(4))
+    : (paymentDate = moment(endDate).add(1, "weeks").isoWeekday(1));
+
+  res.json({ paymentDate: moment(paymentDate).format("DD-MM-YYYY") });
+});
+
+// Assignment 2
+router.post("/calculateSettlementAmount", function (req, res, next) {
+  //add changes below
+  var total = 0;
+  req.body.forEach(
+    (ticket) => (total += ticket.price * (1 - ticket.MDR / 100))
+  );
+  // rounded up to 2d.p
+  const roundedUpSum = Math.ceil(total * 100) / 100;
+
+  res.json({ totalSum: roundedUpSum });
+});
 
 /*
 
@@ -48,30 +66,101 @@ Ticket
   "currency" : "SGD",
   "travelAgentName" : "SPLIT-TEST-AGENT01"
 }
-
-
 Provide a solution to restart the app instance if it crashes.
-
-
-
 */
 
+router.get("/", function (req, res, next) {
+  db.all(
+    "SELECT ticketId, price, MDR, currency, travelAgentName FROM tickets",
+    (err, rows) => {
+      if (err) {
+        return res.status(500).json({ message: err });
+      }
+      res.status(200).json({
+        tickets: rows,
+      });
+    }
+  );
+});
 
+router.post("/", function (req, res, next) {
+  const { ticketId, price, MDR, currency, travelAgentName } = req.body;
+  db.serialize(() => {
+    db.run(
+      "CREATE TABLE IF NOT EXISTS tickets (ticketId TEXT PRIMARY KEY, price DECIMAL, MDR DECIMAL, currency TEXT, travelAgentName TEXT )"
+    ).run(
+      "INSERT INTO tickets VALUES (?, ?, ?, ?, ?)",
+      [ticketId, price, MDR, currency, travelAgentName],
+      (err) => {
+        if (err) {
+          return res.status(500).json({ message: err });
+        }
+        res.status(200).json({
+          message: `Insert Successful for ticket with ID ${ticketId}`,
+        });
+      }
+    );
+  });
+});
+
+router.put("/:id", function (req, res, next) {
+  const { id } = req.params;
+  const { price } = req.body;
+
+  db.serialize(() => {
+    db.run(
+      `UPDATE tickets SET price = ? WHERE ticketId = ?`,
+      [price, id],
+      (err) => {
+        if (err) {
+          return res.status(500).json({ message: err });
+        }
+        res.status(200).json({
+          message: `Update Successful for ticket with ID ${id}`,
+        });
+      }
+    );
+  });
+});
+
+router.delete("/:id", function (req, res, next) {
+  const { id } = req.params;
+
+  db.serialize(() => {
+    db.run(`DELETE from tickets WHERE ticketId = ?`, [id], (err) => {
+      if (err) {
+        return res.status(500).json({ message: err });
+      }
+      res.status(200).json({
+        message: `Delete Successful for ticket with ID ${id}`,
+      });
+    });
+  });
+});
 
 /*
 Assignment 4
 Ensure the nodejs app process restart itself when it crash
+
+Solution: 
+- For development env, nodemon is a suitable utility for monitoring changes in codes and automatically 
+restarting the serve to reflect the changes 
+- For staging and production env, process manager such as PM2 or Forever can keep application up and running on any instances. 
+    - Easy to setup
+    - Automated restart
+    - Error logging
+- Else, we can explore init systems such as systemd and Upstart to handle cases where server restarts
+
 */
 
 //Custom GET API that will crash the app
-router.get('/crashApp', function(req, res, next) {
-    let totalSum = []
-    while(true){
-        let temp = {"test": 123, "data": [1,2,4,56,23,23,]}
-        totalSum.push(temp)
-    }
-    res.json({"message":"This will not be return as app will crash"})
+router.get("/crashApp", function (req, res, next) {
+  let totalSum = [];
+  while (true) {
+    let temp = { test: 123, data: [1, 2, 4, 56, 23, 23] };
+    totalSum.push(temp);
+  }
+  res.json({ message: "This will not be return as app will crash" });
 });
-
 
 module.exports = router;
